3a2216cb20dbdbca21ec37b968f6361a1b542b2e
Filzah Iqbal <filzah.iqbal08@gmail.com>
2026-07-06 15:06:13 -0700

    dna analyzer: nucleotide counter, GC content, sequence validator

--- week2dictionaries.py
+++ week2dictionaries.py
@@ -1,4 +1,5 @@
-codon_table = {"ATG": "Methionine", "TAA": "Stop", "GCC": "Alanine", "TGG":"Tryptophan"}
-
-for codon, amino_acid in codon_table.items():
-    print(codon, "codes for", amino_acid)
+codon_table = {"ATG": "Methionine", "TAA": "Stop"}
+if "ATG" in codon_table:
+    print("Found:", codon_table["ATG"])
+else:
+    print("Not found")
